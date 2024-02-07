79fb2755692a5009acefbab25e2b0b7e5b0411c6
sammyy99 <samarthbhardwaj1999@gmail.com>
2024-02-07 22:53:21 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import {createBrowserRouter,RouterProvider} from "react-router-dom"
+import Loginpage from "./Components/Loginpage";
+import Homepage from "./Components/Homepage";
 
-function App() {
+const App = ()=> {
+
+ const appRouter = createBrowserRouter([
+  {path:"/",element:<Loginpage/>},
+  {path:"/SP-mnt", element:<Homepage/>}
+])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={appRouter}/>
   );
 }
 
